75909288b9a49876fc4f14db17c3b1c218091492
Michael VanderHelm <michaelv@attunix.com>
2014-10-27 08:48:52 -0700

    Nunit tests

---
+++ WebApplication1/WebApplication1.Web.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Web.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Foo()
+        {
+            Assert.Pass();
+        }
+    }
+}
