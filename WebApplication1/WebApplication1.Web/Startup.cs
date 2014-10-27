2fdbc99b4909b6049d68457c5a610fb1754bfb7d
Michael VanderHelm <michaelv@attunix.com>
2014-10-27 07:59:04 -0700

    initial

---
+++ WebApplication1/WebApplication1.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1.Web.Startup))]
+namespace WebApplication1.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
